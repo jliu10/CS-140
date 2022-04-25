50fd6c8eb13b20b52eddf296c5b044fb26adbbb2
Justin Liu <exovitejl@gmail.com>
2022-04-24 20:11:00 -0400

    Update compare()

--- hws/hw07/RemainingRangeComparator.java
+++ hws/hw07/RemainingRangeComparator.java
@@ -2,6 +2,16 @@ class RemainingRangeComparator implements java.util.Comparator<CarFunctions>
 {
 	public int compare(CarFunctions o1, CarFunctions o2)
 	{
-		return 0;
+		if( o1.getRemainingRangeInMiles() < o2.getRemainingRangeInMiles() )
+		{
+			return -1;
+		}
+
+		if( o1.getRemainingRangeInMiles() > o2.getRemainingRangeInMiles() )
+		{
+			return 1;
+		}
+
+		return o1.getId().compareTo(o2.getId());
 	}
 }
