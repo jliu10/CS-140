f0040bc30c51f43f2fafccfdbd142f4edf01a043
Justin Liu <exovitejl@gmail.com>
2022-04-24 20:18:01 -0400

    Add constructor

--- hws/hw07/liu_Car.java
+++ hws/hw07/liu_Car.java
@@ -9,6 +9,13 @@ class liu_Car implements CarFunctions
 	private final int fuelCapacityInGallons;
 	private double currentFuelInGallons;
 
+	liu_Car(String d, int fE, int fC, double cF) {
+		id = d;
+		fuelEconomyInMilesPerGallon = fE;
+		fuelCapacityInGallons = fC;
+		currentFuelInGallons = cF;
+	}
+
 	public String toString()
 	{
 		return getId() + "\t" + getFuelEconomyInMilesPerGallon() + "\t" + getFuelCapacityInGallons() + "\t" + getCurrentFuelInGallons() + "\t" + getTotalRangeInMiles() + "\t" + getRemainingRangeInMiles();
