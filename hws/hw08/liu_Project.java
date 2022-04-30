748990761bb477a0b8fa2ddbdcb8ecb950165bc7
Justin Liu <exovitejl@gmail.com>
2022-04-30 00:41:34 -0400

    Add class with main() from lab12

---
+++ hws/hw08/liu_Project.java
@@ -0,0 +1,125 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class liu_Project {
+    private static int width = 750;
+    private static int height = 350;
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setPreferredSize(new Dimension(width, height));
+		f.setMinimumSize(new Dimension(width, height));
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // top menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem readSortFile = new JMenuItem("Read sort file");
+        JMenuItem readSearchFile = new JMenuItem("Read search file");
+        JMenuItem exitFile = new JMenuItem("Exit");
+        MenuItemActionListener readSortFileIAL = new MenuItemActionListener(readSortFile);
+        MenuItemActionListener readSearchFileIAL = new MenuItemActionListener(readSearchFile);
+        MenuItemActionListener exitFileIAL = new MenuItemActionListener(exitFile);
+        readSortFile.addActionListener(readSortFileIAL);
+        readSearchFile.addActionListener(readSearchFileIAL);
+        exitFile.addActionListener(exitFileIAL);
+        menuBar.add(fileMenu);
+        fileMenu.add(readSortFile);
+        fileMenu.add(readSearchFile);
+        fileMenu.add(exitFile);
+
+        // left button panel
+        JPanel leftButtonPanel = new JPanel();
+        leftButtonPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        GridBagLayout leftGridBagLayout = new GridBagLayout();
+        leftButtonPanel.setLayout(leftGridBagLayout);
+        leftButtonPanel.setMinimumSize(new Dimension(330,350));
+        GridBagConstraints leftButtonPanelConstraints = new GridBagConstraints();
+
+        // left buttons and labels
+        JButton sortIntsButton = new JButton("sort ints");
+        JLabel sortIntsLabel = new JLabel("sort ints");
+
+
+
+        // right button panel
+        JPanel rightButtonPanel = new JPanel();
+        rightButtonPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        GridBagLayout rightGridBagLayout = new GridBagLayout();
+        rightButtonPanel.setLayout(rightGridBagLayout);
+        rightButtonPanel.setMinimumSize(new Dimension(330,350));
+
+        // main button panel
+        JPanel mainButtonPanel = new JPanel();
+        mainButtonPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        BoxLayout boxLayout = new BoxLayout(mainButtonPanel, BoxLayout.X_AXIS);
+        mainButtonPanel.setLayout(boxLayout);
+        mainButtonPanel.add(leftButtonPanel);
+        mainButtonPanel.add(rightButtonPanel);
+
+        // main panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(menuBar, BorderLayout.NORTH);
+        mainPanel.add(mainButtonPanel, BorderLayout.CENTER);
+
+        f.setContentPane(mainPanel);
+
+        f.validate();
+		f.setVisible(true);
+    }
+
+    // action listener for the buttons
+    static class ButtonActionListener implements ActionListener
+    {
+        // the button associated with the action listener, so that we can
+        // share this one class with multiple buttons
+        private JButton b;
+
+        ButtonActionListener(JButton b)
+        {
+            this.b = b;
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("action performed on " + b.getText() + " button");
+        }
+    }
+
+    // action listener for the menu items
+    static class MenuItemActionListener implements ActionListener
+    {
+        // the menu item associated with the action listener, so that we can
+        // share this one class with multiple menu items
+        private JMenuItem m;
+
+        MenuItemActionListener(JMenuItem m)
+        {
+            this.m = m;
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("action performed on " + m.getText() + " menu item");
+
+            // if exit is selected from the file menu, exit the program
+            if( m.getText().toLowerCase().equals("exit") )
+            {
+                System.exit(0);
+            }
+
+            // if color is selected from the edit menu, put a popup on the screen
+            // saying something
+            if( m.getText().toLowerCase().equals("color") )
+            {
+                Object[] options = {"OK"};
+                javax.swing.JOptionPane.showOptionDialog(null, "This is unimplemented,\nclick OK to continue",
+                        "Warning", javax.swing.JOptionPane.DEFAULT_OPTION,
+                        javax.swing.JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+            }
+        }
+    }
+}
