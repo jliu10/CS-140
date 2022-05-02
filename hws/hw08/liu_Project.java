4428cfc9b2eb1c6171ff7acd6fec027be46f0dea
Justin Liu <exovitejl@gmail.com>
2022-05-01 21:16:15 -0400

    Populate rightButtonPanel

--- hws/hw08/liu_Project.java
+++ hws/hw08/liu_Project.java
@@ -172,6 +172,136 @@ class liu_Project {
         GridBagLayout rightGridBagLayout = new GridBagLayout();
         rightButtonPanel.setLayout(rightGridBagLayout);
         rightButtonPanel.setMinimumSize(new Dimension(330,350));
+        GridBagConstraints rightButtonPanelConstraints = new GridBagConstraints();
+
+        // right buttons and labels
+        JButton searchSortedIntsButton = new JButton("search sorted ints");
+        searchSortedIntsButton.setEnabled(false);
+        searchSortedIntsButton.addActionListener( new ButtonActionListener(searchSortedIntsButton) );
+        JLabel searchSortedIntsLabel = new JLabel("no result");
+        JButton searchBstButton = new JButton("search bst");
+        searchBstButton.setEnabled(false);
+        searchBstButton.addActionListener( new ButtonActionListener(searchBstButton) );
+        JLabel searchBstLabel = new JLabel("no result");
+        JButton searchTreeSetButton = new JButton("search tree set");
+        searchTreeSetButton.setEnabled(false);
+        searchTreeSetButton.addActionListener( new ButtonActionListener(searchTreeSetButton) );
+        JLabel searchTreeSetLabel = new JLabel("no result");
+        JButton searchPriorityQButton = new JButton("search priority queue");
+        searchPriorityQButton.setEnabled(false);
+        searchPriorityQButton.addActionListener( new ButtonActionListener(searchPriorityQButton) );
+        JLabel searchPriorityQLabel = new JLabel("no result");
+        JButton searchHashSetButton = new JButton("search hashset");
+        searchHashSetButton.setEnabled(false);
+        searchHashSetButton.addActionListener( new ButtonActionListener(searchHashSetButton) );
+        JLabel searchHashSetLabel = new JLabel("no result");
+        JButton searchArrayListButton = new JButton("search arraylist");
+        searchArrayListButton.setEnabled(false);
+        searchArrayListButton.addActionListener( new ButtonActionListener(searchArrayListButton) );
+        JLabel searchArrayListLabel = new JLabel("no result");
+        JButton searchSortedArrayButton = new JButton("search sorted arraylist");
+        searchSortedArrayButton.setEnabled(false);
+        searchSortedArrayButton.addActionListener( new ButtonActionListener(searchSortedArrayButton) );
+        JLabel searchSortedArrayLabel = new JLabel("no result");
+        JButton searchArrayButton = new JButton("search array");
+        searchArrayButton.setEnabled(false);
+        searchArrayButton.addActionListener( new ButtonActionListener(searchArrayButton) );
+        JLabel searchArrayLabel = new JLabel("no result");
+
+        rightButtonPanelConstraints.weightx = 1;
+        rightButtonPanelConstraints.weighty = 1;
+        rightButtonPanelConstraints.fill = GridBagConstraints.NONE;
+        rightButtonPanelConstraints.anchor = GridBagConstraints.LINE_START;
+        // sort ints
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 0;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchSortedIntsButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 0;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchSortedIntsLabel, rightButtonPanelConstraints);
+        // add to bst
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 1;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchBstButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 1;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchBstLabel, rightButtonPanelConstraints);
+        // add to treeset
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 2;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchTreeSetButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 2;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchTreeSetLabel, rightButtonPanelConstraints);
+        // add to priority queue
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 3;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchPriorityQButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 3;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchPriorityQLabel, rightButtonPanelConstraints);
+        // add to hashset
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 4;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchHashSetButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 4;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchHashSetLabel, rightButtonPanelConstraints);
+        // add to arraylist
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 5;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchArrayListButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 5;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchArrayListLabel, rightButtonPanelConstraints);
+        // add to sorted arraylist
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 6;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchSortedArrayButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 6;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchSortedArrayLabel, rightButtonPanelConstraints);
+        // add to array
+        rightButtonPanelConstraints.gridx = 0;
+        rightButtonPanelConstraints.gridy = 7;
+        rightButtonPanelConstraints.gridwidth = 1;
+        rightGridBagLayout.setConstraints(searchArrayButton, rightButtonPanelConstraints);
+        rightButtonPanelConstraints.gridx = 1;
+        rightButtonPanelConstraints.gridy = 7;
+        rightButtonPanelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        rightGridBagLayout.setConstraints(searchArrayLabel, rightButtonPanelConstraints);
+
+        // add buttons to rightButtonPanel
+        rightButtonPanel.add(searchSortedIntsButton);
+        rightButtonPanel.add(searchSortedIntsLabel);
+        rightButtonPanel.add(searchBstButton);
+        rightButtonPanel.add(searchBstLabel);
+        rightButtonPanel.add(searchTreeSetButton);
+        rightButtonPanel.add(searchTreeSetLabel);
+        rightButtonPanel.add(searchPriorityQButton);
+        rightButtonPanel.add(searchPriorityQLabel);
+        rightButtonPanel.add(searchHashSetButton);
+        rightButtonPanel.add(searchHashSetLabel);
+        rightButtonPanel.add(searchArrayListButton);
+        rightButtonPanel.add(searchArrayListLabel);
+        rightButtonPanel.add(searchSortedArrayButton);
+        rightButtonPanel.add(searchSortedArrayLabel);
+        rightButtonPanel.add(searchArrayButton);
+        rightButtonPanel.add(searchArrayLabel);
 
         // main button panel
         JPanel mainButtonPanel = new JPanel();
